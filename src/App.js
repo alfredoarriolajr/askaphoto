2c7de9b0edd167a0628be27ee7a2530c8918df88
alfredoarriolajr <alfredoarriolajr@gmail.com>
2021-05-20 00:15:24 -0700

    App.js update

--- src/App.js
+++ src/App.js
@@ -87,21 +87,3 @@ function App() {
   }
 
 export default App;
-
-// import logo from './logo.svg';
-// import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Coming Soon!
-//         </p>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
